f64446ccb8917f42ee421503c296a194ecbb6f45
mariamsanoko <mariam.sanoko@gmail.com>
2025-08-27 20:14:31 +0200

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,3 +1,13 @@
 import axios from "axios";
 
 export const LEMON_SQUEEZY_ENDPOINT = "https;//api.lemonsqueezy.com/v1/";
+
+export const lemonsqueezyApiInstace = axios.create({
+   baseURL: LEMON_SQUEEZY_ENDPOINT,
+   headers: {
+    Accept: "application/vnd.api+json",
+    "Content-type": "application/vnd.api+json",
+    Authorization:'Bear ${process.env.LEMON_SQUEEZY_API_KEY}',
+   },
+
+});
