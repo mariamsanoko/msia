20b46dbebd24f940c2f8d175b5cfcb9933664064
mariamsanoko <mariam.sanoko@gmail.com>
2025-08-29 12:11:11 +0200

    Update route.js

--- ls-yt/app/api/purchaseProduct/route.js
+++ ls-yt/app/api/purchaseProduct/route.js
@@ -1,14 +1,36 @@
+import { lemonSqueezyApiInstance } from "@/utils/axios";
+
 export const dynamic ="force-dynamic";
 
 export async function POST(req) {
     try {
         const reqData = await req.json();
 
-        if (!ReqData.productId) 
+        if (!reqData.productId) 
             return Response.json(
-            { message: "An rroer occured"},
+            { message: "product ID is required" },
             { status: 400} 
-         );                
+         );     
+        
+         const response  = await lemonSqueezyApiInstance.post("/checkout", {
+            data: {
+                type: "checkouts",
+                relationships: {
+                    store: {
+                        data: {
+                            type: "stores",
+                            id: "2"
+                    },
+                },
+                variant: {
+                    data: {
+                        type: "variants",
+                        id: "2",
+                    },
+                },
+            },
+        },
+    });
     } catch (error) {
         console.error(error);
         Response.json({ message: "An rroer occured"}, { status: 500});    
