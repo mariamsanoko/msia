fe2e189a181ec1e55a4b6a88794d66be68cf766d
mariamsanoko <mariam.sanoko@gmail.com>
2025-09-12 17:53:35 +0200

    delete install supabase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  site: 'https://msia.github.io',
-  base: '/msia'
+  site: "https://astro-supabase-auth.vercel.app",
+  output: "server",
+  adapter: vercel(),
+  integrations: [tailwind(), solidJs()],
 });
