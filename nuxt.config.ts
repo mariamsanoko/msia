06eeea589f50b1eaddff65279944d821585dcce4
mariamsanoko <mariam.sanoko@gmail.com>
2025-09-19 18:02:41 +0200

    t

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
 export default defineNuxtConfig({
   modules: ["@nuxtjs/supabase"],
- 
+
   runtimeConfig: {
     public: {
       supabaseUrl: process.env.SUPABASE_URL,
-      supabaseKey: process.env.SUPABASE_KEY
-    }
-  }
+      supabaseKey: process.env.SUPABASE_KEY,
+    },
+  },
+
+  nitro: {
+    compatibilityDate: "2025-09-19", // ⚡ Ajoute cette ligne
+  },
 })
