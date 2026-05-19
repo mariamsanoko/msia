e349d8f9d66c18f84e7ae375201e1b01d0a40c70
Sanoko Mariam <75573846+mariamsanoko@users.noreply.github.com>
2026-05-19 18:10:48 +0200

    Disable Nuxt devtools in production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-09-19",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ["@nuxtjs/supabase"],
   css: ["~/assets/css/main.css"],
   runtimeConfig: {
